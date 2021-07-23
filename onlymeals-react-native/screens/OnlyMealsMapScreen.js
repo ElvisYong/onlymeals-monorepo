08e5ae727e6f548c2fffe36c63bbaf288d49e298
elvisyong <elvisyong@gmail.com>
2021-07-24 02:36:19 +0800

    Add FlatList to consume list of data

--- onlymeals-react-native/screens/OnlyMealsMapScreen.js
+++ onlymeals-react-native/screens/OnlyMealsMapScreen.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react'
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-import { Flex, VStack } from 'native-base';
+import { Box, Text, FlatList, ScrollView, Pressable } from 'native-base';
 import * as Location from 'expo-location';
 
 const OnlyMealsMapScreen = ({ navigation }) => {
@@ -54,9 +54,40 @@ const OnlyMealsMapScreen = ({ navigation }) => {
         style={{ flex: 1 }}
         userLocationUpdateInterval={500}
       />
-      <Flex>
-
-      </Flex>
+      <FlatList
+        flex={1}
+        bg="white"
+        data={[{
+          name: 'My location',
+          vicinity: 'Near me',
+          place_id: '1'
+        }, {
+          name: 'Location 2',
+          vicinity: 'far',
+          place_id: '2'
+        }]} renderItem={({ item }) => (
+          <Box>
+            <Pressable
+              onPress={() => console.log("Do something")}
+              alignItems="center"
+              bg="white"
+              borderBottomColor="trueGray.200"
+              borderBottomWidth={1}
+              justifyContent="center"
+              height={50}
+              underlayColor={"#AAA"}
+              _pressed={{
+                bg: "trueGray.200"
+              }}
+              py={8}
+            >
+              <Text>{item.name}</Text>
+              <Text>{item.vicinity}</Text>
+            </Pressable>
+          </Box>
+        )}
+        keyExtractor={(item) => item.place_id}
+      />
     </>
   )
 };
