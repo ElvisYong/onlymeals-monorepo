29fd2fa9765977b3b2b21bc5c4e30b28d8298bca
elvisyong <elvisyong@gmail.com>
2021-07-23 23:48:24 +0800

    Add location

--- onlymeals-react-native/screens/OnlyMealsMapScreen.js
+++ onlymeals-react-native/screens/OnlyMealsMapScreen.js
@@ -1,12 +1,11 @@
 import React, { useState, useEffect, useContext } from 'react'
-import { Button, Box } from 'native-base';
-import { AuthContext } from '../App'
-import firebase from '../firebase/firebaseConfig';
-import MapView from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import { StyleSheet, Dimensions, View } from 'react-native';
-
+import * as Location from 'expo-location';
 
 const OnlyMealsMapScreen = ({ navigation }) => {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
   const [region, setRegion] = useState({
     latitude: 37.7749,
     longitude: -122.4194,
@@ -14,26 +13,39 @@ const OnlyMealsMapScreen = ({ navigation }) => {
     longitudeDelta: 0.0421
   })
 
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  useEffect(() => {
+    if (location !== null) {
+      let region = {
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.0522,
+        longitudeDelta: 0.0421
+      };
+      setRegion(region);
+    }
+  }, [location])
+
   return (
     <MapView
+      provider={PROVIDER_GOOGLE}
       region={region}
       onRegionChangeComplete={(region) => setRegion(region)}
       style={{ flex: 1 }}
     />
   )
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  mapStyle: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  },
-});
 
 export default OnlyMealsMapScreen;
