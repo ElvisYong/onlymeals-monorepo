d991cffc14f35eeb9ad7f3b619ec12256bd06865
elvisyong <elvisyong@gmail.com>
2021-07-24 08:02:03 +0800

    Add chat screen into eatery

--- onlymeals-react-native/screens/SelectEateryScreen.js
+++ onlymeals-react-native/screens/SelectEateryScreen.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { Stack } from '../App'
 import OnlyMealsMapScreen from './OnlyMealsMapScreen'
 import FindKakiScreen from './FindKakiScreen'
+import ChatScreen from './ChatScreen'
 
 const SelectEateryScreen = () => {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Map" component={OnlyMealsMapScreen} />
       <Stack.Screen name="FindKaki" component={FindKakiScreen} />
+      <Stack.Screen name="Chat" component={ChatScreen} />
     </Stack.Navigator>
   )
 }
